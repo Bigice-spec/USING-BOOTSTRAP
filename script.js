062651cb5a86b209b8d70449e5d9b15f8cefcd29
Bigice-spec <kenbigice2015@gmail.com>
2020-12-10 09:30:04 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//let targetDiv = document.querySelector('.scrollable');
+//targetDiv.scrollTop = targetDiv.scrollHeight;
+
+//JQuery
+
+//$(document).ready(function(){
+ //   $('.scrollable').scrollTop( $('.scrollable')[0].scrollHeight);
+//});
